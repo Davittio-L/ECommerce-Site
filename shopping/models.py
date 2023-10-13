d789fefbc2f30748d59b1cfb408210cb7611e059
Colten Bayles <cbayles@outlook.com>
2023-10-13 19:58:56 -0400

    V1.2 Creation of Folders & Files     Creation of Customer & Order in models.py

--- shopping/models.py
+++ shopping/models.py
@@ -1,4 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    in_stock = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
