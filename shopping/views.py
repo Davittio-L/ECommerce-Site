dc2cc9779c8090702d0216c1646e3f82511bf146
Colten Bayles <cbayles@outlook.com>
2023-10-14 23:36:04 -0400

    V1.4 - Added views/URL pathing for app and main URL.py

--- shopping/views.py
+++ shopping/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def store(request):
+    context = {}
+    return render(request, 'shopping/store', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'shopping/cart', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'shopping/checkout', context)
--- webapp/settings.py
+++ webapp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #My Apps
-    'shopping',
+    'shopping.apps.StoreConfig',
     'webapp',
 ]
 
