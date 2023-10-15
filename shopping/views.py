c3f79223a23a6ecb8dd32d3154f246cb37bc7fec
Colten Bayles <cbayles@outlook.com>
2023-10-15 15:47:42 -0400

    V1.4.3 - added 'DIRS': [os.path.join(BASE_DIR, 'shopping/Templates')], to templates along with html tags to views.py. Bug fixed

--- shopping/views.py
+++ shopping/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def store(request):
     context = {}
-    return render(request, 'shopping/store', context)
+    return render(request, 'shopping/store.html', context)
 
 def cart(request):
     context = {}
-    return render(request, 'shopping/cart', context)
+    return render(request, 'shopping/cart.html', context)
 
 def checkout(request):
     context = {}
-    return render(request, 'shopping/checkout', context)
+    return render(request, 'shopping/checkout.html', context)
