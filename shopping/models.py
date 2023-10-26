61605121148bd266da2d47001389575862de14cc
Colten Bayles <cbayles@outlook.com>
2023-10-26 19:51:11 -0400

    V 1.8.1 Added Digital aspect to product models.py editing store.html to show 6 space grid instead of 3 space grid.

--- shopping/models.py
+++ shopping/models.py
@@ -14,6 +14,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     in_stock = models.BooleanField(default=False, null=True, blank=True)
+    digital = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
         return self.name
