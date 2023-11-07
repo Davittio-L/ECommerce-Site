93fa9c66f22cff985694e15224ae3a57e075b0b0
Colten Bayles <cbayles@outlook.com>
2023-11-06 21:39:25 -0500

    V  1.9.6 Creat or Update Order & orderitem add as well as action login.

--- shopping/views.py
+++ shopping/views.py
@@ -34,8 +34,26 @@ def checkout(request):
     return render(request, 'shopping/cart.html', context)
 
 def updateItem(request):
-     data = json.loads(request.body)
-     productId = data['productId']
-     action = data['action']
-     print('Action:', action)
-     print('Product:', productId)
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+	print('Action:', action)
+	print('Product:', productId)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <= 0:
+		orderItem.delete()
+
+	return JsonResponse('Item was added', safe=False)
