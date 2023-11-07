84b2cf18b9b59fc335f64d0e42b972e382e4ca42
Colten Bayles <cbayles@outlook.com>
2023-11-06 21:25:00 -0500

    V 1.9.5 - Editied current code eliminating typos and mistakes

--- shopping/Static/js/cart.js
+++ shopping/Static/js/cart.js
@@ -2,12 +2,12 @@ var updateBtns = document.getElementsByClassName('update-cart')
 
 for (i = 0; i < updateBtns.length; i++) {
     updateBtns[i].addEventListener('click', function(){
-        var productID = this.dataset.productID
+        var productId = this.dataset.product
         var action = this.dataset.action
         console.log('productId:', productId, 'Action', action)
 
         console.log('USER:', user)
-        if (user == 'AnonymousUser') {
+        if (user == 'AnonymousUser'){
             console.log('User is not authenticated')
         }else{
             updateUserOrder(productId, action)
@@ -21,22 +21,21 @@ function updateUserOrder(productId, action){
 }
 
 function updateUserOrder(productId, action){
-    console.log('User is authenticated, sending data...')
+	console.log('User is authenticated, sending data...')
 
-        var url = '/update_item/'
+		var url = '/update_item/'
 
-        fetch (url, {
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json',
-                'X-CSRFToken':csrftoken,
-            },
-            body:JSON.stringify({'productId':productId, 'action':action})
-        })
-        .then((response) => {
-            return response.json();
-         })
-         .then((data) => {
-             location.reload()
-         });
- }
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+			}, 
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    console.log('Data:', data)
+		});
+}
