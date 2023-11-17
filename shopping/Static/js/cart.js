dd3b47ce907e3e0501dfc7222040aea92eff22ee
Colten Bayles <cbayles@outlook.com>
2023-11-16 20:46:58 -0500

    V 2.1.1 = created function addCookieItem to cart.js showin add/decrease in item quantity.

--- shopping/Static/js/cart.js
+++ shopping/Static/js/cart.js
@@ -5,19 +5,15 @@ for (i = 0; i < updateBtns.length; i++) {
         var productId = this.dataset.product
         var action = this.dataset.action
         console.log('productId:', productId, 'Action', action)
+		console.log('USER:', user)
 
-        console.log('USER:', user)
         if (user == 'AnonymousUser'){
-            console.log('User is not authenticated')
+            addCookieItem(productId, action)
         }else{
             updateUserOrder(productId, action)
         }
         
     })
-}
-
-function updateUserOrder(productId, action){
-    console.log('User is authenticated, sening data...')
 }
 
 function updateUserOrder(productId, action){
@@ -29,6 +25,7 @@ function updateUserOrder(productId, action){
 			method:'POST',
 			headers:{
 				'Content-Type':'application/json',
+				'X-CSRFToken':crsftoken,
 			}, 
 			body:JSON.stringify({'productId':productId, 'action':action})
 		})
@@ -38,4 +35,30 @@ function updateUserOrder(productId, action){
 		.then((data) => {
 		    console.log('Data:', data)
 		});
+}
+
+function addCookieItem(productId, action) {
+	console.log('User is not authenticated')
+
+	if (action == 'add') {
+		if (cart[productId] == undefined) {
+			cart[productId] = {'quantity' :1}
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+
+	if (action == 'remove') {
+		cart [productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0) {
+			console.log('Item should be deleted')
+			delete cart[productId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie = 'cart' + JSON.stringify(cart) + ";domain=path=/"
+
+	location.reload()
 }
